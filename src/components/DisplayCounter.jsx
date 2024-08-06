a633bfd18523133af0c18c1aa7ebc0e95c64de38
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-08-06 15:17:17 +0530

    Local setup done

---
+++ src/components/DisplayCounter.jsx
@@ -0,0 +1,10 @@
+
+const DisplayCounter = () => {
+  return (
+    <>
+      <p className="lead mb-4">Counter current value.</p>
+    </>
+  )
+}
+
+export default DisplayCounter;
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import {createStore} from "redux";
+
+const INITIAL_VALUE = {
+  counter: 0
+}
+
+const counterReducer = (store = INITIAL_VALUE, action) => {
+  return store;
+}
+
+const counterStore = createStore(counterReducer);
+
+export default counterStore;
